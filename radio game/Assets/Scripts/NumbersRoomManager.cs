4444cec3bb741de8a69bb02918cf815091d3fcc0
ryley/home <33923649+ryle-e@users.noreply.github.com>
2025-11-18 01:44:18 +1100

    slider, cards, sounds, grass, more post-processing

---
+++ radio game/Assets/Scripts/NumbersRoomManager.cs
@@ -0,0 +1,113 @@
+using RyleRadio.Components;
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Audio;
+using Random = UnityEngine.Random;
+
+public class NumbersRoomManager : MonoBehaviour
+{
+    private static readonly KeyCode[] NUM_KEYS = { KeyCode.Alpha0, KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4, KeyCode.Alpha5, KeyCode.Alpha6, KeyCode.Alpha7, KeyCode.Alpha8, KeyCode.Alpha9 };
+
+    [SerializeField] private string code = "172086904532";
+
+    [SerializeField] private RadioOutput radio;
+    [SerializeField] private AudioMixer mixer;
+    [SerializeField] private TMP_Text tuneIndicator;
+
+    [SerializeField] private List<TMP_Text> codeCards;
+    [SerializeField] private List<AudioSource> cardAudios;
+    [SerializeField] private List<AudioClip> typeClips;
+    [SerializeField] private List<AudioClip> backspaceClips;
+
+    [SerializeField] private Transform indicatorPeg;
+    [SerializeField] private Vector2 pegRange;
+
+    [SerializeField] private float tuneSpeed;
+
+    private string currentNum = "";
+
+    public void UpdateEffects(float _tune)
+    {
+        mixer.SetFloat("FlangeRate", (_tune / 1000f) * 10f);
+
+        tuneIndicator.text = radio.DisplayTune.ToString();
+
+        Vector3 locPos = indicatorPeg.localPosition;
+        locPos.z = Mathf.Lerp(pegRange.x, pegRange.y, radio.Tune01);
+        indicatorPeg.localPosition = locPos;
+    }
+
+    private void Start()
+    {
+        UpdateEffects(radio.Tune);
+    }
+
+    private void Update()
+    {
+        float mult = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift) 
+            ? 2f 
+            : Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl) 
+                ? 0.3f 
+                : 1f;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            radio.Tune -= tuneSpeed * mult * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            radio.Tune += tuneSpeed * mult * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Backspace))
+            Backspace();
+
+        foreach (KeyCode key in NUM_KEYS)
+        {
+            if (Input.GetKeyDown(key))
+            {
+                AddToCode(Array.IndexOf(NUM_KEYS, key));
+            }
+        }
+    }
+
+    private void AddToCode(int _value)
+    {
+        if (currentNum.Length < codeCards.Count)
+            currentNum = $"{currentNum}{_value.ToString()}";
+
+        cardAudios[_value].PlayOneShot(typeClips[Random.Range(0, typeClips.Count)]);
+
+        UpdateCodeCards();
+    }
+
+    private void Backspace()
+    {
+        if (currentNum.Length > 0)
+            currentNum = currentNum[0 .. (currentNum.Length - 1)];
+
+        cardAudios[currentNum.Length].PlayOneShot(backspaceClips[Random.Range(0, backspaceClips.Count)]);
+
+        UpdateCodeCards();
+    }
+
+    private void UpdateCodeCards()
+    {
+        for (int i = 0; i < currentNum.Length; i++)
+        {
+            codeCards[i].text = currentNum[i].ToString();
+        }
+
+        for (int i = currentNum.Length; i < codeCards.Count; i++)
+        {
+            codeCards[i].text = "";
+        }
+
+        if (currentNum.Length == codeCards.Count)
+        {
+            if (currentNum == code)
+                Debug.Log("code correct!");
+            else
+                Debug.Log("code wrong!");
+        }
+    }
+}
---
+++ radio game/Assets/Scripts/ShakeController.cs
@@ -0,0 +1,171 @@
+using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+// a component that allows the shaking of objects, split across each axis of position and rotation
+public class ShakeController : MonoBehaviour
+{
+    // the current (0, 1) amount of shaking
+    private float magnitude;
+    public float Magnitude
+    {
+        get => magnitude;
+        set => magnitude = Mathf.Clamp01(value);
+    }
+
+    // whether or not the decay should be halted as it's being overriden
+    public bool OverridingDecay { get; private set; } = false;
+
+    // the transform to shake
+    // NOTE: this should be an empty parent of whatever should be shaking- it should never be moved to a local position other than (0,0,0)
+    [SerializeField] private Transform shakeTransform;
+
+    [Header("Initial Values")]
+    [SerializeField] private float initialMagnitude;
+
+    [Header("Offset")]
+    [SerializeField] private bool useLocalPos;
+    [SerializeField] private Vector3 posShakePower; // the power of the positional shake
+    [SerializeField] private Vector3 posShakeSpeed; // the speed of the positional shake
+
+    [Header("Rotation")]
+    [SerializeField] private bool useLocalRot;
+    [SerializeField] private Vector3 rotShakePower; // the power of the rotational shake
+    [SerializeField] private Vector3 rotShakeSpeed; // the speed of the rotational shake
+
+    [Header("Decay")]
+    [SerializeField] private float decaySpeed; // the speed at which magnitude decays
+
+    // the (x, y) positions to get perlin noise for the shake from, x will change over time- this is how the shake is smoothed, as perlin noise is smooth
+    private Vector2[] axesPos;
+    private Vector2[] axesRot;
+
+    private Vector3 basePos;
+    private Vector3 baseRot;
+
+    // gets perlin noise from (-1, 1) rather than (0, 1)
+    public static float PerlinAxis(float _x, float _y)
+    {
+        return (Mathf.PerlinNoise(_x, _y) * 2) - 1;
+    }
+
+    private void Start()
+    {
+        Magnitude = initialMagnitude;
+
+        // randomises the initial perlin noise coordinates for the positional shake
+        axesPos = new Vector2[3]
+        {
+            new ( -9999, Random.Range(-9999, 9999) ),
+            new ( -9999, Random.Range(-9999, 9999) ),
+            new ( -9999, Random.Range(-9999, 9999) ),
+        };
+
+        // randomises the initial perlin noise coordinates for the rotational shake
+        axesRot = new Vector2[3]
+        {
+            new ( -9999, Random.Range(-9999, 9999) ),
+            new ( -9999, Random.Range(-9999, 9999) ),
+            new ( -9999, Random.Range(-9999, 9999) ),
+        };
+
+        CaptureBaseRotation();
+    }
+
+    public void Update()
+    {
+        // =========== TESTING =========== 
+        // shakes everything
+        if (Input.GetKeyDown(KeyCode.C))
+            Magnitude = 1;
+        // =========== =========== 
+
+        if (Magnitude == 0)
+            return;
+
+        // initialises blank position and rotation vectors
+        Vector3 oPos = Vector3.zero;
+        Vector3 oRot = Vector3.zero;
+
+        // evaluates the perlin noise for positions per-axis and applies power to it
+        for (int i = 0; i < 3; i++)
+            oPos[i] = PerlinAxis(axesPos[i].x, axesPos[i].y) * posShakePower[i];
+
+        // evaluates the perlin noise for rotations per-axis and applies power to it
+        for (int i = 0; i < 3; i++)
+            oRot[i] = PerlinAxis(axesRot[i].x, axesRot[i].y) * rotShakePower[i];
+
+        // applies the shaking to the transform
+        shakeTransform.localPosition = basePos + oPos * Magnitude;
+        shakeTransform.localRotation = Quaternion.Euler(baseRot + oRot * Magnitude);
+
+        // moves the position perlin coordinates by speed
+        for (int i = 0; i < 3; i++)
+            axesPos[i].y += posShakeSpeed[i] * Time.deltaTime;
+
+        // moves the rotation perlin coordinates by speed
+        for (int i = 0; i < 3; i++)
+            axesRot[i].y += rotShakeSpeed[i] * Time.deltaTime;
+
+        // reduces the magnitude by the decay speed, lessening it over time
+        if (!OverridingDecay)
+            Magnitude -= decaySpeed * Time.deltaTime;
+    }
+
+    public void CaptureBaseRotation()
+    {
+        basePos = transform.localPosition;
+        baseRot = transform.localRotation.eulerAngles;
+    }
+
+    public void Shake(float _power)
+    {
+        Magnitude += _power;
+    }
+
+    public void SingleVarCurveOverride(AnimationCurve _curve)
+    {
+        OverridingDecay = true;
+        StartCoroutine(SingleVarCurveOverrideRoutine(_curve));
+    }
+
+    // overrides the normal decay speed, allowing animated shaking over time (e.g a low to high curve)
+    public void CurveOverride(float _time, AnimationCurve _curve)
+    {
+        OverridingDecay = true;
+        StartCoroutine(CurveOverrideRoutine(_time, _curve));
+    }
+
+    private IEnumerator SingleVarCurveOverrideRoutine(AnimationCurve _curve)
+    {
+        float t = 0;
+        float time = _curve[_curve.length - 1].time;
+
+        while (t < time)
+        {
+            // sets magnitude to the curve value rather than normal decay
+            Magnitude = _curve.Evaluate(t / time);
+            t += Time.deltaTime;
+
+            yield return null;
+        }
+
+        OverridingDecay = false;
+    }
+
+    private IEnumerator CurveOverrideRoutine(float _time, AnimationCurve _curve)
+    {
+        float t = 0;
+
+        while (t < _time)
+        {
+            // sets magnitude to the curve value rather than normal decay
+            Magnitude = _curve.Evaluate(t / _time);
+            t += Time.deltaTime;
+
+            yield return null;
+        }
+
+        OverridingDecay = false;
+    }
+}
