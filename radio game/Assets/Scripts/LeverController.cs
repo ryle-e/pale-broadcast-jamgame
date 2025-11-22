0eda1ffaa36abc82d6ba6d31b0c9c3f37872e9d4
ryley/home <33923649+ryle-e@users.noreply.github.com>
2025-11-23 01:52:33 +1100

    some audio changes to the levers, also created temp dust particles

--- radio game/Assets/Scripts/LeverController.cs
+++ radio game/Assets/Scripts/LeverController.cs
@@ -3,13 +3,14 @@ using NaughtyAttributes;
 using NUnit.Framework;
 using RyleRadio;
 using RyleRadio.Components;
+using RyleRadio.Tracks;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.Audio;
 
-public class LeverController : MonoBehaviour
+public class LeverController : RyleRadio.Components.Base.RadioComponentDataAccessor
 {
     private const float ANIM_TIME = 2;
 
@@ -21,12 +22,13 @@ public class LeverController : MonoBehaviour
     [SerializeField] private AudioSource hitAudio;
     [SerializeField] private AudioSource digUpAudio;
     [SerializeField] private AudioSource digDownAudio;
+    [SerializeField] private AudioSource clickAudio;
     [SerializeField] private WhisperManager whisperManager;
     [SerializeField] private ShakeController modelShake;
+    [SerializeField] private ParticleSystem dustParticles;
 
     [SerializeField] private RadioInteractor interactor;
-    [SerializeField] private RadioData data;
-    [SerializeField, Dropdown("data.TrackNames")] private string trackName;
+    [SerializeField, Dropdown("TrackNames")] private string track;
 
     [SerializeField] private Renderer floorPattern;
     [SerializeField] private new Light light;
@@ -42,6 +44,11 @@ public class LeverController : MonoBehaviour
 
     private bool interacted = false;
     private bool highlighted = false;
+
+    private bool hasPlayedHit = false;
+
+    private RadioTrackWrapper wrapper;
+    private float wrapperGain;
 
     public bool Highlighted
     {
@@ -79,7 +86,7 @@ public class LeverController : MonoBehaviour
         floorPatternMat.SetFloat("_Cutoff", 0.6f);
         floorPatternMat.EnableKeyword("_ALPHATEST_ON");
 
-        Hide();
+        Hide(false);
     }
 
     public void Show()
@@ -94,7 +101,7 @@ public class LeverController : MonoBehaviour
         digUpAudio.Play();
     }
 
-    public void Hide()
+    public void Hide(bool _playAudio = true)
     {
         modelShake.Magnitude = .3f;
 
@@ -103,7 +110,8 @@ public class LeverController : MonoBehaviour
         model.transform.DOLocalMoveY(-3, ANIM_TIME, false);
         floorPatternMat.DOFloat(0.6f, "_Cutoff", ANIM_TIME);
 
-        digDownAudio.Play();
+        if (_playAudio)
+            digDownAudio.Play();
     }
 
     public void Interact()
@@ -113,17 +121,41 @@ public class LeverController : MonoBehaviour
 
         interacted = true;
 
+        float fadeOutTime = 1.48f;
+
+        if (data.TryGetTrack(track, out wrapper, false))
+        {
+            wrapperGain = wrapper.Gain;
+
+            DOVirtual.Float(wrapper.Gain, 0, fadeOutTime, f => wrapper.Gain = f).SetEase(Ease.InCubic);
+        }
+
         interactAudio.Play();
-        leverPivot.DOLocalRotate(new Vector3(0, 0, -30), 1.48f).SetEase(Ease.InCubic).OnComplete(() => StartCoroutine(LeverHit()));
+        clickAudio.Play();
+
+        whisperManager.StopWhispersAudioOnly();
+
+        leverPivot.DOLocalRotate(new Vector3(0, 0, -30), 1.48f).SetEase(Ease.InCubic).OnStepComplete(() =>
+        {
+            if (!hasPlayedHit && leverPivot.localEulerAngles.z > 300 && leverPivot.localEulerAngles.z < 335) 
+            { 
+                StartCoroutine(LeverHit()); 
+                wrapper.Gain = wrapperGain;
+            }
+        });
     }
 
     private IEnumerator LeverHit()
     {
-        //hitAudio.Play();
+        if (hasPlayedHit)
+            yield break;
+
+        hasPlayedHit = true;
+
+        hitAudio.Play();
+        modelShake.Magnitude = .1f;
 
         interactor.Stop();
-
-        whisperManager.StopWhispersAudioOnly();
 
         /*if (data.TryGetTrack(trackName, out var track, false))
         {
@@ -134,4 +166,7 @@ public class LeverController : MonoBehaviour
 
         Hide();
     }
+
+    protected override void AssignToTrack(RadioTrackWrapper _track) { }
+    protected override void RemoveFromTrack(RadioTrackWrapper _track) { }
 }
